5367674f1b98fede01e167ffbd8ac281ae3fc1b6
Critickal Web <zgtale1@hotmail.com>
2022-11-17 02:01:18 -0300

    x

--- Abd/HMTL/CONTROLADORES/CAnime.php
+++ Abd/HMTL/CONTROLADORES/CAnime.php
@@ -5,22 +5,25 @@ require_once "Require.php";
 class ControladorAnime extends ModeloAnime
 {
 
-    public function listarAnime(){
-        return $this->get_All();
+    public function setAnime($IDA,$IDN,$NAME){
+        $IDA = strtoupper($IDA);
+        $this->set($IDA,$IDN,$NAME);
     }
-
-    public function listarAnimeWhere(string $where){
-        return $this->get_All_Where($where);
+///////////////////Borrar
+    public function borrarAnime($inicial, $ID){
+        $inicial = strtoupper($inicial);
+        $this->borrar($inicial, $ID);
     }
-
-    public function contarAnime(){
-        return $this->contar();
+///////////////////Search
+    public function buscarAnimeByName($Name){
+        return $this->buscar($Name);
     }
-
-    public function contarAnimeWhere(string $where){
-        return $this->contarWhere($where);
+///////////////////Edit
+    public function editarAnime($inicial, $ID, $name){
+        $inicial = strtoupper($inicial);
+        $this->editar($inicial, $ID, $name);
     }
-
+///////////////////Get
     public function getAnimeById($Inicial, $ID){
         $Inicial = strtoupper($Inicial);
         return $this->getByID($Inicial, $ID);
@@ -30,32 +33,29 @@ class ControladorAnime extends ModeloAnime
         $Inicial = strtoupper($Inicial);
         return $this->getByInicial($Inicial);
     }
-        
-    public function buscarAnimeByName($Name){
-        return $this->buscar($Name);
+///////////////////Group
+    public function groupAnimeByInicial(string $Valor){
+        return $this->groupByInicial("IdAnime", $Valor);
+    }
+///////////////////Listar
+    public function listarAnimeByAll(){
+        return $this->get_All();
     }
 
-    public function setAnime($IDA,$IDN,$NAME){
-        $IDA = strtoupper($IDA);
-        $this->set($IDA,$IDN,$NAME);
+    public function listarAnimeByInicial($inicial){
+        return $this->getByInicial($Inicial);
     }
 
-    public function borrarAnime($inicial, $ID){
-        $inicial = strtoupper($inicial);
-        $this->borrar($inicial, $ID);
+    public function listarAnimeWhere(string $where){
+        return $this->get_All_Where($where);
+    }
+///////////////////////////////
+    public function contarAnime(){
+        return $this->contar();
     }
 
-    public function editarAnime($inicial, $ID, $name){
-        $inicial = strtoupper($inicial);
-        $this->editar($inicial, $ID, $name);
+    public function contarAnimeWhere(string $where){
+        return $this->contarWhere($where);
     }
-
-    public function groupAnimeByInicial(string $where){
-        return $this->groupBy("IdAnime", $where);
-    }
-    public function groupAnimeByInicialPrimer($valor){
-        return $this->groupBy("IdAnime", "1")[$valor-1];
-    }
-
 }
 ?>
--- Abd/HMTL/CONTROLADORES/MODELOS/MAnime.php
+++ Abd/HMTL/CONTROLADORES/MODELOS/MAnime.php
@@ -7,21 +7,48 @@ class ModeloAnime extends ModeloConexion
 {
     public function set($IDA,$IDN,$NAME)
     {
-        $sql = "INSERT INTO anime (idAnime, idNumero, nombre) 
+        $sql = "INSERT INTO anime (Inicial, Id, nombre) 
         VALUES ('$IDA','$IDN','$NAME')";
         $this->sentencia($sql);
     }
-
+///////////////////Borrar
+    public function borrar($Inicial, $ID){
+        $sql = "DELETE FROM `anime` WHERE `IdAnime`='$Inicial' AND `Id`=$ID";
+        $this->sentencia($sql);
+    }
+///////////////////Search
+    public function buscar($Name){
+        $sql="SELECT * FROM `anime` WHERE nombre LIKE '%$Name%'";
+        return $this->get($sql);
+    }
+///////////////////Edit
+    public function editar($Inicial, $ID, $name){
+        $sql = "UPDATE `anime` 
+        SET `nombre`='$name' 
+        WHERE IdAnime='$Inicial' AND Id=$ID";
+        $this->sentencia($sql);
+    }
+///////////////////Get
     public function getByID($Inicial, $ID){
-        $sql="SELECT * from anime where idAnime='$Inicial' AND IdNumero = $ID";
+        $sql="SELECT * from anime where Inicial='$Inicial' AND Id = $ID";
         return $this->get($sql);
     }
 
-    public function getByIinicial($Inicial){
-        $sql="SELECT * from anime where idAnime='$Inicial'";
+    public function getByInicial($Inicial){
+        $sql="SELECT * from anime where Inicial='$Inicial'";
         return $this->get($sql);
     }
 
+    public function getByNombre($Nombre){
+        $sql="SELECT * from anime where Nombre='$Nombre'";
+        return $this->get($sql);
+    }
+///////////////////Group
+    public function groupByInicial($Inicial){
+        $sql = "SELECT Inicial, count(Inicial) FROM `anime` WHERE Inicial='$Inicial' GROUP BY Inicial";
+        return $this->get($sql) ;
+    }
+///////////////////////////////
     public function get_ALL(){
         $sql="SELECT * from anime";
         return $this->get($sql);
@@ -41,29 +68,6 @@ class ModeloAnime extends ModeloConexion
         $sql="SELECT * from anime WHERE $where";
         return $this->count($sql);
     }
-
-    public function buscar($Name){
-        $sql="SELECT * FROM `anime` WHERE nombre LIKE '%$Name%'";
-        return $this->get($sql);
-    }
-
-    public function borrar($Inicial, $ID){
-        $sql = "DELETE FROM `anime` WHERE `IdAnime`='$Inicial' AND `IdNumero`=$ID";
-        $this->sentencia($sql);
-    }
-
-    public function editar($Inicial, $ID, $name){
-        $sql = "UPDATE `anime` 
-        SET `nombre`='$name' 
-        WHERE IdAnime='$Inicial' AND IdNumero=$ID";
-        $this->sentencia($sql);
-    }
-
-    public function groupBy($criterio, $where){
-        $sql = "SELECT $criterio, count($criterio) FROM `anime` WHERE $where GROUP BY $criterio";
-        return $this->get($sql) ;
-    }
-
 }
 
 ?>
