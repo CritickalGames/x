632b09d1367f52fad4e7b0dcf532b9e27e872dce
Critickal Web <zgtale1@hotmail.com>
2022-11-18 05:37:24 -0300

    Listar por inicial

--- Abd/HMTL/PHP/Anime/Inicial.php
+++ Abd/HMTL/PHP/Anime/Inicial.php
@@ -2,8 +2,8 @@
 require_once "D:/xampp/htdocs/x/Abd/HMTL/PHP/USER/Admin.php";
     $objUserAdmin = new UserAdmin();
 
-    //$inicial = $_POST['inicial'];
-    $inicial = "";
+    $inicial = $_POST['inicial'];
+    //$inicial = "";
 
     if (($inicial!=NULL)) {
         echo json_encode($objUserAdmin->listarAnimePorInicial("$inicial"));
